501bacd2dbca6301930ca9d3d61bb1bca4b94acc
lmeazi <user0x3e8@icloud.com>
2022-01-17 22:02:26 -0800

    Added notes

--- objects.cpp
+++ objects.cpp
@@ -8,6 +8,14 @@ void printPointer(std:initializer_list<int> vals)
         std:cout << p << " is located at address:  " << *p << "/n ";} 
 }
 
+class UndefinedName{
+    public: 
+        X& Copyoperator = (const X& lvalue);
+        x& Moveoperator= (const X&& rvalue);
+       
+}
+
+
 
 int  main (){
 printPointer(9);
