660440c2d3135342d268cb9535525bcdbe33a528
lmeazi <user0x3e8@icloud.com>
2022-01-17 20:43:02 -0800

    цпп

---
+++ objects.cpp
@@ -0,0 +1,18 @@
+import <iostream>
+import <String>
+
+
+void printPointer(std:initializer_list<int> vals)
+{
+    for (auto p=vals.begin(); p! =vals.end)();++p){
+        std:cout << p << " is located at address:  " << *p << "/n ";} 
+}
+
+
+int  main (){
+printPointer(9);
+retun 1 
+}
+
+
+
