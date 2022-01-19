0af67e70125ae1824ce54496d8f686a1751fac5d
lmeazi <user0x3e8@icloud.com>
2022-01-18 19:12:07 -0800

    Notes for Jan 18

--- objects.cpp
+++ objects.cpp
@@ -1,6 +1,6 @@
 import <iostream>
 import <String>
-
+###########################
 
 void printPointer(std:initializer_list<int> vals)
 {
@@ -8,6 +8,8 @@ void printPointer(std:initializer_list<int> vals)
         std:cout << p << " is located at address:  " << *p << "/n ";} 
 }
 
+
+####################
 class UndefinedName{
     public: 
         X& Copyoperator = (const X& lvalue);
@@ -15,8 +17,22 @@ class UndefinedName{
            
 }
 
+########################3
+void printMultiplevalue(){
+}
 
 
+template <Typename T, typename... Types>
+
+void printMultiplevalue(constT& firstArg, const Types&... args) {
+    std:cout<< firstArg << std:endl;
+    printMultiplevalue(args...);
+}
+
+
+#This will print the values with a tempalte if one or more arguments are used
+
+##################################
 int  main (){
 std:cout << UR" String literal, should 'i stop using backspace on sample code?";
 printPointer(9);
